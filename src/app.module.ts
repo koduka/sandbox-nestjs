49688c07c75daa6f2d49078bb1c5169c015c489d
koduka <koduka0715@gmail.com>
2021-05-17 08:16:17 +0900

    DBの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
