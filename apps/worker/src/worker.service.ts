1e9bda8d407a5e199bf697eae1c1511ce0eb924d
koduka <koduka0715@gmail.com>
2021-06-11 08:40:32 +0900

    worker調整

--- apps/worker/src/worker.service.ts
+++ apps/worker/src/worker.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class WorkerService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class WorkerService {}
